ab7f49f23999e7e9882482c634a1586ff9fd5e55
jeanne <jeanne.lefevre@hotmail.fr>
2022-11-08 16:59:53 +0100

    solved no css

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,18 @@
 // Iteration #1
+//
+require("./../db/index");
+const Drone = require("./../models/Drone.model.js");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+seed();
+
+async function seed() {
+  await Drone.deleteMany();
+  await Drone.create(drones);
+  mongoose.connection.close();
+}
